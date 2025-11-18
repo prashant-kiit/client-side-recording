816df6b2c6631fb397457c04dc4daab696814fd2
prashant-m4 <prashantsingh090798@gmail.com>
2025-11-18 08:14:35 +0530

    feat: added backend call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,42 @@
-import { useState, useEffect, useEffectEvent } from 'react'
-import './App.css'
-import { MILESTONES } from './config'
+import { useState, useEffect, useEffectEvent } from "react";
+import "./App.css";
+import { MILESTONES } from "./config";
 
 function App() {
-  const [isRecording, setIsRecording] = useState(false)
-  const [count, setCount] = useState(0)
-  const [milestones, setMilestones] = useState([])
+  const [isRecording, setIsRecording] = useState(false);
+  const [count, setCount] = useState(0);
+  const [milestones, setMilestones] = useState([]);
 
   const handleStartRecording = useEffectEvent((isRecording, count) => {
     if (isRecording) {
+      // timer
       const timeout = setTimeout(() => {
-        setCount(count + 1)
+        setCount(count + 1);
         if (MILESTONES[count]) {
-          setMilestones([...milestones, MILESTONES[count]])
+          setMilestones([...milestones, MILESTONES[count]]);
         }
-      }, 1000)
-      return () => clearTimeout(timeout)
+      }, 1000);
+      return () => clearTimeout(timeout);
     }
-  })
+  });
 
   useEffect(() => {
-    handleStartRecording(isRecording, count)
-  }, [isRecording, count])
+    handleStartRecording(isRecording, count);
+  }, [isRecording, count]);
 
   return (
     <>
-      <h1>Client Side Recording</h1>  
+      <h1>Client Side Recording</h1>
       <div className="card">
-        <button onClick={() => setIsRecording((isRecording) => !isRecording)}>
-          {isRecording ? 'Stop Client Side Recording' : 'Start Client Side Recording'}
+        <button
+          onClick={() => {
+            setIsRecording((isRecording) => !isRecording);
+            fetch("http://localhost:3000/client-side-record/123");
+          }}
+        >
+          {isRecording
+            ? "Stop Client Side Recording"
+            : "Start Client Side Recording"}
         </button>
         <div className="count-container">
           <p>Count is {count}</p>
@@ -42,7 +50,7 @@ function App() {
         )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
