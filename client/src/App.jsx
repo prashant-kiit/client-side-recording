0efb2bf766c5b22c14d9ac3c78f1c8247c926636
prashant-m4 <prashantsingh090798@gmail.com>
2025-11-18 07:36:58 +0530

    feat: added start/stop recording button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { MILESTONES } from './config'
 
 function App() {
+  const [isRecording, setIsRecording] = useState(false)
   const [count, setCount] = useState(0)
   const [milestones, setMilestones] = useState([])
 
-  setTimeout(() => {
-    setCount(count + 1)
-    if (MILESTONES[count]) {
-      setMilestones([...milestones, MILESTONES[count]])
+  useEffect(() => {
+    if (isRecording) {
+      setTimeout(() => {
+        setCount(count + 1)
+        if (MILESTONES[count]) {
+          setMilestones([...milestones, MILESTONES[count]])
+        }
+      }, 1000)
     }
-  }, 1000)
+  }, [isRecording, count, milestones])
 
   return (
     <>
-      <h1>Client Side Recording</h1>
+      <h1>Client Side Recording</h1>  
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
+        <button onClick={() => setIsRecording((isRecording) => !isRecording)}>
+          {isRecording ? 'Stop Client Side Recording' : 'Start Client Side Recording'}
         </button>
+        <div className="count-container">
+          <p>Count is {count}</p>
+        </div>
         {milestones?.length > 0 && (
-          <div>
+          <div className="milestones-container">
             {milestones.map((milestone, index) => (
               <p key={index}>{milestone}</p>
             ))}
